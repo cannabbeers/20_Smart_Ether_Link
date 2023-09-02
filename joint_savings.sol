4c1d1bf1b8232eba848ec1c9c1dad501bdf99b46
Mark Beers <126819603+cannabbeers@users.noreply.github.com>
2023-09-01 18:45:24 -0700

    Update joint_savings.sol
    code seems to compile w/o errors, but cannot find the JavaScriptVM 'Environment' not in the dropdown as described, missed something and searching for answer - I have manually updated repo by copying and pasting from my remix window and looking for solution to set environment properly

--- joint_savings.sol
+++ joint_savings.sol
@@ -1,3 +1,209 @@
+// copied my code from remix here to manually update the repo through github, stuck in the end of 'step 2' right now, need to find the way to compile and deploy in JavaScript VM 
+
+pragma solidity ^0.5.0;
+
+// Define a new contract named `JointSavings`
+contract JointSavings {
+
+    /*
+    Inside the new contract define the following variables:
+    - Two variables of type `address payable` named `accountOne` and `accountTwo`
+    - A variable of type `address public` named `lastToWithdraw`
+    - Two variables of type `uint public` named `lastWithdrawAmount` and `contractBalance`.
+    */
+    // YOUR CODE HERE!
+
+    address payable public accountOne;  // Account 1
+    address payable public accountTwo;  // Account 2
+    address public lastToWithdraw;  // Last account to withdraw
+    uint public lastWithdrawAmount;  // Last amount withdrawn
+    uint public contractBalance;  // Current contract balance
+    /*
+    Define a function named **withdraw** that will accept two arguments.
+    - A `uint` variable named `amount`
+    - A `payable address` named `recipient`
+    */
+    function withdraw(uint amount, address payable recipient) public {
+
+        /*
+        Define a `require` statement that checks if the `recipient` is equal to either `accountOne` or `accountTwo`. The `requiere` statement returns the text `"You don't own this account!"` if it does not.
+        */
+        // `require` statement added to validate recipient
+        require(
+            recipient == accountOne || recipient == accountTwo,
+            "You don't own this account!"
+        );
+        /*
+        Define a `require` statement that checks if the `balance` is sufficient to accomplish the withdraw operation. If there are insufficient funds, the text `Insufficient funds!` is returned.
+        */
+        // added require to update `lastToWithdraw` - checks for sufficient funds
+        require(
+            amount <= contractBalance,
+            "Insufficient funds!"
+        );
+        /*
+        Add and `if` statement to check if the `lastToWithdraw` is not equal to (`!=`) to `recipient` If `lastToWithdraw` is not equal, then set it to the current value of `recipient`.
+        */
+        // check if the `lastToWithdraw` is not equal to (`!=`) to `recipient`
+        if (lastToWithdraw != recipient) {
+            lastToWithdraw = recipient;
+        }
+
+        // Call the `transfer` function of the `recipient` and pass it the `amount` to transfer as an argument.
+        // YOUR CODE HERE!
+         recipient.transfer(amount);
+
+        // Set  `lastWithdrawAmount` equal to `amount`
+        // YOUR CODE HERE!
+        lastWithdrawAmount = amount;
+
+        // Call the `contractBalance` variable and set it equal to the balance of the contract by using `address(this).balance` to reflect the new balance of the contract.
+        // YOUR CODE HERE!
+        
+        contractBalance = address(this).balance;  // Call the `contractBalance` variable and set it equal to the balance of the contract
+    }
+
+    // Define a `public payable` function named `deposit`.
+    function deposit() public payable {    // Define a `public payable` function named `deposit`
+
+        /*
+        Call the `contractBalance` variable and set it equal to the balance of the contract by using `address(this).balance`.
+        */
+        // YOUR CODE HERE!
+         contractBalance = address(this).balance;
+    }
+
+    /*
+    Define a `public` function named `setAccounts` that receive two `address payable` arguments named `account1` and `account2`.
+    */
+    function setAccounts(address payable account1, address payable account2) public {
+
+        // Set the values of `accountOne` and `accountTwo` to `account1` and `account2` respectively.
+        // YOUR CODE HERE!
+        accountOne = account1; // Set the values of `accountOne`
+        accountTwo = account2; // Set the values of `accountTwo`
+    }
+
+    /*
+    Finally, add the **default fallback function** so that your contract can store Ether sent from outside the deposit function.
+
+
+    FIXED THIS ERROR - forgot to add "external"
+    joint_savings.sol:106:5: SyntaxError: No visibility specified. Did you intend to add "external"?
+    function() payable {
+    ^ (Relevant source part starts here and spans across multiple lines).
+    
+    */
+    // YOUR CODE HERE!
+    function() external payable {
+        // Update contractBalance
+        contractBalance = address(this).balance;
+    }
+}
+
+
+
+/* pragma solidity ^0.5.0;
+
+// Define a new contract named `JointSavings`
+contract JointSavings {
+
+    /*
+    Inside the new contract define the following variables:
+    - Two variables of type `address payable` named `accountOne` and `accountTwo`
+    - A variable of type `address public` named `lastToWithdraw`
+    - Two variables of type `uint public` named `lastWithdrawAmount` and `contractBalance`.
+    */
+    // YOUR CODE HERE!
+
+    address payable public accountOne;  // Account 1
+    address payable public accountTwo;  // Account 2
+    address public lastToWithdraw;  // Last account to withdraw
+    uint public lastWithdrawAmount;  // Last amount withdrawn
+    uint public contractBalance;  // Current contract balance
+    /*
+    Define a function named **withdraw** that will accept two arguments.
+    - A `uint` variable named `amount`
+    - A `payable address` named `recipient`
+    */
+    function withdraw(uint amount, address payable recipient) public {
+
+        /*
+        Define a `require` statement that checks if the `recipient` is equal to either `accountOne` or `accountTwo`. The `requiere` statement returns the text `"You don't own this account!"` if it does not.
+        */
+        // `require` statement added to validate recipient
+        require(
+            recipient == accountOne || recipient == accountTwo,
+            "You don't own this account!"
+        );
+        /*
+        Define a `require` statement that checks if the `balance` is sufficient to accomplish the withdraw operation. If there are insufficient funds, the text `Insufficient funds!` is returned.
+        */
+        // added require to update `lastToWithdraw` - checks for sufficient funds
+        require(
+            amount <= contractBalance,
+            "Insufficient funds!"
+        );
+        /*
+        Add and `if` statement to check if the `lastToWithdraw` is not equal to (`!=`) to `recipient` If `lastToWithdraw` is not equal, then set it to the current value of `recipient`.
+        */
+        // check if the `lastToWithdraw` is not equal to (`!=`) to `recipient`
+        if (lastToWithdraw != recipient) {
+            lastToWithdraw = recipient;
+        }
+
+        // Call the `transfer` function of the `recipient` and pass it the `amount` to transfer as an argument.
+        // YOUR CODE HERE!
+         recipient.transfer(amount);
+
+        // Set  `lastWithdrawAmount` equal to `amount`
+        // YOUR CODE HERE!
+        lastWithdrawAmount = amount;
+
+        // Call the `contractBalance` variable and set it equal to the balance of the contract by using `address(this).balance` to reflect the new balance of the contract.
+        // YOUR CODE HERE!
+        
+        contractBalance = address(this).balance;
+    }
+
+    // Define a `public payable` function named `deposit`.
+    function deposit() public payable {
+
+        /*
+        Call the `contractBalance` variable and set it equal to the balance of the contract by using `address(this).balance`.
+        */
+        // YOUR CODE HERE!
+         contractBalance = address(this).balance;
+    }
+
+    /*
+    Define a `public` function named `setAccounts` that receive two `address payable` arguments named `account1` and `account2`.
+    */
+    function setAccounts(address payable account1, address payable account2) public {
+
+        // Set the values of `accountOne` and `accountTwo` to `account1` and `account2` respectively.
+        // YOUR CODE HERE!
+        accountOne = account1;
+        accountTwo = account2;
+    }
+
+    /*
+    Finally, add the **default fallback function** so that your contract can store Ether sent from outside the deposit function.
+    
+    joint_savings.sol:106:5: SyntaxError: No visibility specified. Did you intend to add "external"?
+    function() payable {
+    ^ (Relevant source part starts here and spans across multiple lines).
+    
+    */
+    // YOUR CODE HERE!
+    function() external payable {
+        // Update contractBalance
+        contractBalance = address(this).balance;
+    }
+}
+
+
+
 /*
 Joint Savings Account
 ---------------------
@@ -83,3 +289,4 @@ contract JointSavings {
     */
     // YOUR CODE HERE!
 }
+*/
